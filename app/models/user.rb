1b7a96d37c9a7400e9ec8ca8d2616450e3ff6ee1
yoshi-miu <68325686+yoshi-miu@users.noreply.github.com>
2020-08-12 16:36:34 +0900

    正規表現を定数に代入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  NAME_KANA_REGEX = /\A[ァ-ン]+\z/.freeze
+
   with_options presence: true do
     validates :nickname
     validates :birthday
     validates :password, length: { minimum: 6 },
-                         format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+                         format: { with: PASSWORD_REGEX }
+    with_options format: { with: NAME_REGEX } do
       validates :last_name
       validates :first_name
     end
-    with_options format: { with: /\A[ァ-ン]+\z/ } do
+    with_options format: { with: NAME_KANA_REGEX } do
       validates :last_name_kana
       validates :first_name_kana
     end
