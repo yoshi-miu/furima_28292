8c184f9db58c8e10cae68a9787c64a5126597515
yoshi-miu <68325686+yoshi-miu@users.noreply.github.com>
2020-08-12 14:59:12 +0900

    rubocopの適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_parmitted_parameters, if: :devise_controller?
 
   private
-  def configure_parmitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
-  end
 
+  def configure_parmitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name,
+                                                       :last_name_kana, :first_name_kana, :birthday])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   with_options presence: true do
     validates :nickname
     validates :birthday
     validates :password, length: { minimum: 6 },
-                         format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
-    with_options format:{with: /\A[ぁ-んァ-ン一-龥]+\z/} do
-    validates :last_name
-    validates :first_name
+                         format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+      validates :last_name
+      validates :first_name
     end
-    with_options format: {with: /\A[ァ-ン]+\z/} do
-    validates :last_name_kana
-    validates :first_name_kana
+    with_options format: { with: /\A[ァ-ン]+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
     end
   end
 end
