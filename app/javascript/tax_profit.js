acde9aec01592dc3993025491dd80e051d7a7128
yoshi-miu <68325686+yoshi-miu@users.noreply.github.com>
2020-08-16 19:19:50 +0900

    Ajaxによる手数料と利益の自動計算機能の実装

---
+++ app/javascript/tax_profit.js
@@ -0,0 +1,32 @@
+function tax_profit() {  
+  const priceForm = document.getElementById("item-price");
+  priceForm.addEventListener("input", (e) =>{
+    const XHR = new XMLHttpRequest();
+    XHR.open("GET", `/items/new`, true);
+    XHR.responseType = "json";
+    XHR.send();
+    XHR.onload = () => {
+      const price = priceForm.value;
+      const addTax = document.getElementById("add-tax-price");
+      const addProfit = document.getElementById("profit");
+      const tax = price * 0.1;
+      addTax.innerHTML = parseInt(tax, 10);
+      const profit = price - tax;
+      addProfit.innerHTML = parseInt(profit, 10);
+  
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+      } else {
+        return null;
+      }
+    };
+    XHR.onerror = () => {
+      alert("Request failed");
+    };
+
+    e.preventDefault();
+
+    
+  });
+}
+window.addEventListener("load", tax_profit);
