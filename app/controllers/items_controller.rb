a3d8adf1e75b176479f77c789587878f01f1bce9
yoshi-miu <68325686+yoshi-miu@users.noreply.github.com>
2020-08-17 09:58:40 +0900

    エラー箇所の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,7 +18,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :price, :description, :genre_id, :status_id,
+    params.require(:item).permit(:image, :name, :price, :description, :genre_id, :status_id,
                                  :bear_id,:area_id, :shipment_id).merge(user_id: current_user.id)
 
   end
