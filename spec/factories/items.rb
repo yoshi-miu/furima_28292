945d6bfbc926de3cd57756b9d6a3d1b3ba8129ae
yoshi-miu <68325686+yoshi-miu@users.noreply.github.com>
2020-08-17 15:48:43 +0900

    itemのダミーデータの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    name { Faker::Lorem.words(number: 1) }
-    price { Faker::Number.number(digits: 5) }
-    description { Faker::Lorem.sentence }
-    genre_id { 2 }
+    name { "ケーキバイキング" }
+    price { 8000 }
+    description { "有名ホテルのケーキが食べ放題" }
+    genre_id { 11 }
     status_id { 2 }
     bear_id { 2 }
-    area_id { 2 }
-    shipment_id { 2 }
+    area_id { 15 }
+    shipment_id { 3 }
     association :user
   end
 end
