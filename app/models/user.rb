5672062c0a1eba0115b11155bb6f12d1bae477f1
yoshi-miu <68325686+yoshi-miu@users.noreply.github.com>
2020-08-19 19:53:56 +0900

    商品削除機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :items
-  has_many :purchases
+  has_many :purchases, dependent: :destroy
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
